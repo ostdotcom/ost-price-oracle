1bf6f80d0564aaacdf975089a04e36e1085a8825
Ashutosh <ashutosh@ost.com>
2018-08-01 18:13:28 +0530

    prettier test

--- tools/geth_checker.js
+++ tools/geth_checker.js
@@ -1,57 +1,49 @@
 'use strict';
 
-const rootPrefix = '..'
-  , InstanceComposer      = require( rootPrefix + "/instance_composer")
-  , logger                = require(rootPrefix + '/helpers/custom_console_logger')
-
-  , fs                    = require('fs')
-
-  ,configStrategyPath     = getConfigStrategyPath(  process.argv )
-  ,configStrategy         = require( configStrategyPath )
-  ,ic                     = new InstanceComposer( configStrategy )
-;
-
-require(rootPrefix + '/lib/web3/providers/factory');
-
-function getConfigStrategyPath( argv ) {
-  const defaultConfigStrategyPath = rootPrefix + "/tools/config_strategy.json",
-        passedStrategyPath        = argv && argv[2] ,  //Config Strategy path as argument.
-        configStrategyPath        = passedStrategyPath || defaultConfigStrategyPath
-  ;
+const getConfigStrategyPath = function(argv) {
+  const defaultConfigStrategyPath = rootPrefix + '/tools/config_strategy.json',
+    passedStrategyPath = argv && argv[2], //Config Strategy path as argument.
+    configStrategyPath = passedStrategyPath || defaultConfigStrategyPath;
   return configStrategyPath;
 };
 
-const performer = async function () {
+const rootPrefix = '..',
+  InstanceComposer = require(rootPrefix + '/instance_composer'),
+  logger = require(rootPrefix + '/helpers/custom_console_logger'),
+  fs = require('fs'),
+  configStrategyPath = getConfigStrategyPath(process.argv),
+  configStrategy = require(configStrategyPath),
+  ic = new InstanceComposer(configStrategy);
 
-  const  web3ProviderFactory = ic.getWeb3ProviderFactory()
-       , web3Provider        = web3ProviderFactory.getProvider('ws')
-  ;
+require(rootPrefix + '/lib/web3/providers/factory');
 
-  const delay = 10 * 1000
-    , timeoutValue = 30 * 60 * 1000
-  ;
+const performer = async function() {
+  const web3ProviderFactory = ic.getWeb3ProviderFactory(),
+    web3Provider = web3ProviderFactory.getProvider('ws');
 
-  var counter = 0
-    , totalTime = counter * delay
-    , isInProcess = false
-  ;
+  const delay = 10 * 1000,
+    timeoutValue = 30 * 60 * 1000;
 
-  setInterval(function () {
+  var counter = 0,
+    totalTime = counter * delay,
+    isInProcess = false;
+
+  setInterval(function() {
     if (totalTime <= timeoutValue) {
       if (isInProcess == false) {
         isInProcess = true;
-        web3Provider.eth.getBlockNumber(function (err, blocknumber) {
+        web3Provider.eth.getBlockNumber(function(err, blocknumber) {
           if (err || blocknumber < 1) {
-            logger.debug("Unable to get blocknumber");
+            logger.debug('Unable to get blocknumber');
           } else {
-            logger.debug("blocknumber", blocknumber);
+            logger.debug('blocknumber', blocknumber);
             process.exit(0);
           }
           isInProcess = false;
         });
       }
     } else {
-      logger.error("GethChecker unable to complete process in time: ", timeoutValue);
+      logger.error('GethChecker unable to complete process in time: ', timeoutValue);
       process.exit(1);
     }
     counter++;
