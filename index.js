8b323d0060e81ace0898d1ee11a42a40c574b11b
Shlok Gilda <shlok@ost.com>
2019-02-01 18:54:55 +0530

    Renamed file.

--- index.js
+++ index.js
@@ -7,10 +7,10 @@
 const rootPrefix = '.',
   AbiBinProvider = require(rootPrefix + '/lib/AbiBinProvider'),
   PriceOracleHelper = require(rootPrefix + '/lib/helpers/setup/PriceOracle'),
-  DeployAndSetInOpsHelper = require(rootPrefix + '/lib/helpers/setup/DeployAndSetInOps');
+  DeployAndSetOpsAndAdminHelper = require(rootPrefix + '/lib/helpers/setup/DeployAndSetOpsAndAdmin');
 
 module.exports = {
   AbiBinProvider: AbiBinProvider,
-  DeployAndSetInOpsHelper: DeployAndSetInOpsHelper,
+  DeployAndSetOpsAndAdminHelper: DeployAndSetOpsAndAdminHelper,
   PriceOracleHelper: PriceOracleHelper
 };
