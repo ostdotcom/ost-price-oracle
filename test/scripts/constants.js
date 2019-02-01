ca9ac13b6527e7ad993f6a9d2a6c5031147971f8
Shlok Gilda <shlok@ost.com>
2019-02-01 15:41:41 +0530

    Test cases related changes.

---
+++ test/scripts/constants.js
@@ -0,0 +1,21 @@
+'use strict';
+/**
+ * Test constants.
+ *
+ * @module test/scripts/constants
+ */
+
+const testConstants = {
+  auxRpcProvider: process.env.OST_AUX_GETH_RPC_PROVIDER,
+  auxWsProvider: process.env.OST_AUX_GETH_WS_PROVIDER,
+  auxOwnerAddress: process.env.OST_AUX_OWNER_ADDR,
+  auxOwnerPassPhrase: process.env.OST_AUX_OWNER_PASSPHRASE,
+  auxAdminAddress: process.env.OST_AUX_ADMIN_ADDR,
+  auxAdminPassPhrase: process.env.OST_AUX_ADMIN_PASSPHRASE,
+  auxOpsAddress: process.env.OST_AUX_OPS_ADDR,
+  auxOpsPassPhrase: process.env.OST_AUX_OPS_PASSPHRASE,
+  auxPrice: process.env.OST_AUX_SET_PRICE,
+  auxChainId: process.env.OST_AUX_CHAIN_ID
+};
+
+module.exports = testConstants;
