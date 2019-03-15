2365f283f50ca1c57db9188215ed7bfc1b3ef974
Shlok Gilda <shlok@ost.com>
2019-03-15 18:45:10 +0530

    Updated constants file.

--- test/scripts/constants.js
+++ test/scripts/constants.js
@@ -15,18 +15,20 @@ const testConstants = {
 
   auxAdminAddress: process.env.OST_AUX_ADMIN_ADDR,
   auxAdminPassPhrase: process.env.OST_AUX_ADMIN_PASSPHRASE,
-  auxAdminPrivateKey: process.env.OST_AUX_OWNER_PVT_KEY,
+  auxAdminPrivateKey: process.env.OST_AUX_ADMIN_PVT_KEY,
 
   auxOpsAddress: process.env.OST_AUX_OPS_ADDR,
   auxOpsPassPhrase: process.env.OST_AUX_OPS_PASSPHRASE,
-  auxOpsPrivateKey: process.env.OST_AUX_OWNER_PVT_KEY,
+  auxOpsPrivateKey: process.env.OST_AUX_OPS_PVT_KEY,
 
   auxPrice: process.env.OST_AUX_SET_PRICE,
-  auxChainId: process.env.OST_AUX_CHAIN_ID
-};
+  auxChainId: process.env.OST_AUX_CHAIN_ID,
 
-testConstants.baseCurrency = 'OST';
-testConstants.usdQuoteCurrency = 'USD';
-testConstants.euroQuoteCurrency = 'EUR';
+  baseCurrency: 'OST',
+  usdQuoteCurrency: 'USD',
+  euroQuoteCurrency: 'EUR',
+
+  tempFilePath: './temp.txt'
+};
 
 module.exports = testConstants;
