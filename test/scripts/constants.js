599efb8b46d2cf1c3963deb7d28beba4cec0d6d0
Shlok Gilda <shlok@ost.com>
2019-03-15 14:58:39 +0530

    Updated required test constants.

--- test/scripts/constants.js
+++ test/scripts/constants.js
@@ -8,12 +8,19 @@
 const testConstants = {
   auxRpcProvider: process.env.OST_AUX_GETH_RPC_PROVIDER,
   auxWsProvider: process.env.OST_AUX_GETH_WS_PROVIDER,
+
   auxOwnerAddress: process.env.OST_AUX_OWNER_ADDR,
   auxOwnerPassPhrase: process.env.OST_AUX_OWNER_PASSPHRASE,
+  auxOwnerPrivateKey: process.env.OST_AUX_OWNER_PVT_KEY,
+
   auxAdminAddress: process.env.OST_AUX_ADMIN_ADDR,
   auxAdminPassPhrase: process.env.OST_AUX_ADMIN_PASSPHRASE,
+  auxAdminPrivateKey: process.env.OST_AUX_OWNER_PVT_KEY,
+
   auxOpsAddress: process.env.OST_AUX_OPS_ADDR,
   auxOpsPassPhrase: process.env.OST_AUX_OPS_PASSPHRASE,
+  auxOpsPrivateKey: process.env.OST_AUX_OWNER_PVT_KEY,
+
   auxPrice: process.env.OST_AUX_SET_PRICE,
   auxChainId: process.env.OST_AUX_CHAIN_ID
 };
--- test/scripts/ost_po_run_chain.sh
+++ test/scripts/ost_po_run_chain.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 DATADIR=$(pwd)/ost-po-chain
-LOCAL_NETWORK_ID="--networkid 20171011"
+LOCAL_NETWORK_ID="--networkid 2001"
 
-geth --datadir "$DATADIR" --rpcport 8545 --gasprice 0 --targetgaslimit 10000000 --etherbase 0 --unlock 0 --password pw --ws --wsport 18545  --wsorigins "*" --rpc --maxpeers 0 $LOCAL_NETWORK_ID --mine --minerthreads 4 --rpcapi net,eth,web3,personal --wsapi net,eth,web3,personal
+geth --datadir "$DATADIR" --rpcport 8545 --gasprice 0 --targetgaslimit 10000000 --etherbase '0x8fb010Abb03f03201d734Ffc64e3600405140D65' --ws --wsport 18545  --wsorigins "*" --rpc --maxpeers 0 --networkid 2001 --mine --minerthreads 4 --rpcapi net,eth,web3,personal --wsapi net,eth,web3,personal
